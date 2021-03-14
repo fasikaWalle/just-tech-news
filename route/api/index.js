5a919c2acb5c7ddfb61d96d47d37b92a8fab3c78
fasikaWalle <fasikabini12@gmail.com>
2021-03-14 17:22:51 -0500

    creating user route

---
+++ route/api/index.js
@@ -0,0 +1,5 @@
+const router=require('express').Router()
+const userRoute=require('./user-route')
+router.use('/',userRoute)
+
+module.exports=router
---
+++ server.js
@@ -0,0 +1,2 @@
+//Create the Database Connection
+const PORT=process.env.PORT||3001
