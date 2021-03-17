fbee9576a20f70d4ea684a82faa6e32bbad2d7f9
fasikaWalle <fasikabini12@gmail.com>
2021-03-16 21:34:48 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,16 @@
 const Sequelize =require('sequelize')
 require('dotenv').config()
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PW,{
-    host:'localhost',
-    dialect:'mysql',
-    port:3306
-})
+if(process.env.JAWSDB_URL){
+    sequelize=new sequelize(process.env.JAWSDB_URL)
+}else{
+    const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PW,{
+        host:'localhost',
+        dialect:'mysql',
+        port:3306
+    }) 
+}
+
 
 //RESTful APIs isn't a language or library to learn like JavaScript or Sequelize, but rather a pattern that developers put to use when building an API.
 module.exports=sequelize
